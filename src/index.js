536b7cd755d725cff159c7cbda487a40267503c9
Brandon Ticona <jbticona2@gmail.com>
2020-12-05 22:19:11 -0400

    cambiando el puerto de la app

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 // puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // conectar a la base de datos
 conectarDB();
@@ -28,6 +28,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 // arrancar la app
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
